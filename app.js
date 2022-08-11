c35b9ee66d9a87c6eedebd038c0117940e4ce5c2
okdaae <dhrekdo5@kakao.com>
2022-08-11 17:50:02 +0900

    ✨ begin node js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs")
+app.use('/static', express.static('static'));
 const port = 8000;
 // localhost:8000/
+
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname + '/test.html');
+    res.render('test');
+})
+app.get('/test1',(req,res)=>{
+    res.render('test1');
+})
+app.get('/test2',(req,res)=>{
+    res.render('test2');
 })
 app.listen(port, ()=>{
     console.log("server open: ", port);
